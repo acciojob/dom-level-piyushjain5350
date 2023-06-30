1f5bf606641cb9670d58b3d391f0b78f26bb0940
piyushjain5350 <126599802+piyushjain5350@users.noreply.github.com>
2023-07-01 00:46:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const list=document.getElementsByTagName("ul")[0];
+const child=list.childNodes;
+const fText=document.getElementById("level").innerText;
+
+var ct=0;
+var finalCt=0;
+child.forEach((e)=>{
+	
+	if(e.innerText===fText){
+		finalCt=ct;
+	}
+	ct++;
+});
+// console.log(Math.floor(finalCt/2));
+alert(`The level of the element is: ${Math.floor(finalCt/2)}`);
+
